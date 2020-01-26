54630b87a31d21b880d1bc0faa30672d29564350
Molly Gilbert <msgilbert389@gmail.com>
2020-01-25 21:58:32 -0600

    adding starter code for shopping list

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://mgilbert:starr389@cluster0-riky8.mongodb.net/test?retryWrites=true&w=majority"
+
+}
---
+++ routes/api/items.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const Item = require('../../models/Item.js')
+
+router.get("/", (req, res) => {
+    Item.find()
+    .sort({date: -1})
+    .then(items => res.json(items))
+})
+
+module.exports = router;
